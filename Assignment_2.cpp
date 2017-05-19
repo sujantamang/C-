63f0996f41caf423ecb9efc63f07f24461b209a3
Sujan Tamang <sujanaytamang@gmail.com>
2017-05-19 16:47:37 -0400

    Assignment_2 update

--- Assignment_2.cpp
+++ Assignment_2.cpp
@@ -1,5 +1,5 @@
 // Sujan Tamang.
-// Assignment-4, Ex-2.
+// Assignment-2
 #include<iostream>
 #include<string>
 #include<iomanip>
@@ -29,3 +29,71 @@ public:
     string getLastName();
     virtual double compAveScore();
 };
+Student::Student(string fname,string lname,int idNo,double mscore, double bscore)
+{
+    id=idNo;
+    firstName=fname;
+    lastName=lname;
+    mathScore=mscore;
+    bioScore=bscore;
+}
+int Student:: getId()
+{
+    return id;
+}
+string Student::getFirstName()
+{
+    return firstName;
+}
+string Student::getLastName()
+{
+    return lastName;
+}
+double Student::compAveScore()
+{
+    return (mathScore+ bioScore)/2.0;
+}
+
+//GradStudent inherits from Student class
+class GradStudent:public Student
+{
+private:
+    double biometricScore;   //additional data member in child class
+public:
+    GradStudent():Student()
+    {
+        biometricScore=0.0;
+    }
+    GradStudent(string fname,string lname,int idNo, double mscore, double bscore,double biometScore)
+            :Student(fname,lname,idNo,mscore,bscore)
+    {
+        biometricScore=biometScore;
+    }
+    double compAveScore();
+
+};
+double GradStudent::compAveScore()
+{
+    return (mathScore + bioScore + biometricScore)/3.0;
+}
+int main()
+{
+    Student a("Amy", "Miller", 1001, 60, 66);
+    Student b("John", "Smith", 1002, 70,77);
+    GradStudent c("Diane", "Myers", 2001, 80, 84, 88);
+    GradStudent d("Scott", "Baker", 2002, 90, 91, 98);
+
+    //vector of pointers to the Student class
+    vector<Student*> things;
+
+    things.push_back(&a);  // push first object, will be position 0
+    things.push_back(&b);  // push second object, will be position 1
+    things.push_back(&c);  // push third object, will be position 3
+    things.push_back(&d);  // push fourth object, will be position 4
+
+    //iterator declaration
+    vector<Student*>::iterator iter;
+
+
+}
+
